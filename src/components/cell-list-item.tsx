3abe3b582c07c948cbc680e4b1f039c11048465a
Dawid <dawid.budaszewski@cord.co>
2021-11-20 15:32:12 +0000

    remove action bar

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,7 +1,6 @@
 import { Cell } from '../state'
 import CodeCell from './code-cell'
 import TextEditor from './text-editor'
-import ActionBar from './action-bar'
 import './cell-list-item.css'
 
 interface CellListItemProps {
@@ -13,21 +12,16 @@ const CellListItem: React.FC<CellListItemProps> = ({ cell, startingCode }) => {
   let child: JSX.Element
   if (cell.type === 'code') {
     child = <>
-      <div className="action-bar-wrapper">
-        <ActionBar id={cell.id}/>
-      </div>
       <CodeCell cell={cell} startingCode={startingCode}/>
     </>
   } else {
     child = <>
       <TextEditor cell={cell} />
-      <ActionBar id={cell.id}/>
     </>
   }
   return (
     <div className='cell-list-item'>
       {child}
-      <ActionBar id={cell.id}/>
     </div>
   )
 }
